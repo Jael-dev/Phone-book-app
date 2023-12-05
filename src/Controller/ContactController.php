da6650e0f11a5a0616a8910d5ca9fab7c8dc8cfb
jael-dev <jaelngaleu@gmail.com>
2023-12-05 15:32:17 +0100

    twig file created

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,11 +8,30 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
+
+    // Route for the list of contacts
+     //
+    // The default here is an empty array of contacts if the list is empty
+    #[Route('/contact', name: 'app_contact', defaults: [])]
     public function index(): Response
     {
-        return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-        ]);
+        $contacts = ['an array of contacts'];
+        return $this->render('contact/index.html.twig', array(
+            'contacts' => $contacts
+        ));
     }
+
+     // Route for a particular contact
+     // Note: Never use the same name for two endpoints
+     #[Route('/contact/{id}', name: 'individual_contact',methods:['GET','HEAD'])]
+     public function singleContact($id): Response
+     {
+     // Change the rendering
+         return $this->render('contact/index.html.twig', [
+             'controller_name' => 'ContactController',
+             'name' => $id
+         ]);
+     }
+
+     // Todo: Make the create, the update and the delete
 }
